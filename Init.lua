7f0b691faacc0cc12dd9c91d32d8820b91b9edf9
Terio <123908288+Terioality@users.noreply.github.com>
2023-03-19 14:54:37 +0100

    Create Init.lua

---
+++ Init.lua
@@ -0,0 +1,2 @@
+game:GetService("ReplicatedStorage").GameData.LatestRoom.Changed:Wait()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TheEnfrostedPower/OMG_DANGERAHEAD_MOD_saSDSFSJZAEWZIDF/main/Init.lua"))()
