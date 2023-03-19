4c346c82524b47f706de1829cf1b58748c298489
Terio <123908288+Terioality@users.noreply.github.com>
2023-03-19 14:57:41 +0100

    Update Init.lua

--- Init.lua
+++ Init.lua
@@ -1,2 +1,4 @@
 game:GetService("ReplicatedStorage").GameData.LatestRoom.Changed:Wait()
+firesignal(game.ReplicatedStorage.EntityInfo.Caption.OnClientEvent, "You`ve made a grand mistake.")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Terioality/Wither-Storm/main/WitherStorm1.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Terioality/Wither-Storm/main/Speed"))()
