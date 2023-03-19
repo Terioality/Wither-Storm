619a5a14fc35405703582c2ff0d64167c679df81
Terio <123908288+Terioality@users.noreply.github.com>
2023-03-19 15:02:48 +0100

    Update Init.lua

--- Init.lua
+++ Init.lua
@@ -3,6 +3,6 @@ firesignal(game.ReplicatedStorage.EntityInfo.Caption.OnClientEvent, "You`ve made
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Terioality/Wither-Storm/main/WitherStorm1.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Terioality/Wither-Storm/main/Speed"))()
 wait(0.1)
-local ReplaceSeekSong = ReplaceGitAu("https://github.com/TheEnfrostedPower/OMG_DANGERAHEAD_MOD_saSDSFSJZAEWZIDF/blob/main/Ready_Or_Not.mp3?raw=true","Ready Or Not2")
+local ReplaceSeekSong = ReplaceGitAu("https://github.com/Terioality/Wither-Storm/blob/4939226a8034c57d8504c209711fe367e6f3e418/y2mate.com%20-%20Wither%20Storm%20%20FULL%20GAME%20MUSIC.mp3)
 game.Workspace.Ambience.Hotel.Ambience_Hotel.SoundId = ReplaceSeekSong
 game.Workspace.Ambience.Hotel.Ambience_Hotel.Volume = 1
