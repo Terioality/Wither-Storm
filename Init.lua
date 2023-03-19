ca75fb395a8dc25608a7c6974f6eaec4f6492182
Terio <123908288+Terioality@users.noreply.github.com>
2023-03-19 14:58:18 +0100

    Update Init.lua

--- Init.lua
+++ Init.lua
@@ -2,3 +2,6 @@ game:GetService("ReplicatedStorage").GameData.LatestRoom.Changed:Wait()
 firesignal(game.ReplicatedStorage.EntityInfo.Caption.OnClientEvent, "You`ve made a grand mistake.")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Terioality/Wither-Storm/main/WitherStorm1.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Terioality/Wither-Storm/main/Speed"))()
+wait(0.1)
+game.Workspace.Ambience.Hotel.Ambience_Hotel.SoundId = "rbxassetid://273398061"
+game.Workspace.Ambience.Hotel.Ambience_Hotel.Volume = 1
